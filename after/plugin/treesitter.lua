5da173c383edc82709d052a70d727ef65a8a2ef8
Zan Rutar <zan@zanrutar.com>
2026-06-24 21:39:56 +0200

    V2

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,22 +1,52 @@
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = {
-      'c',
-      'lua',
-      'vim',
-      'vimdoc',
-      'query',
-      'javascript',
-      'typescript',
-      'html',
-      'css'
-      },
-    callback = function()
-      -- syntax highlighting, provided by Neovim
-      vim.treesitter.start()
-      -- -- folds, provided by Neovim
-      -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-      -- vim.wo.foldmethod = 'expr'
-      -- -- indentation, provided by nvim-treesitter
-      -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+packadd("nvim-treesitter")
+
+local setup_treesitter = function()
+  local treesitter = require("nvim-treesitter")
+  treesitter.setup()
+  local ensure_installed = {
+    "vim",
+    "vimdoc",
+    "rust",
+    "c",
+    "cpp",
+    "go",
+    "html",
+    "css",
+    "javascript",
+    "json",
+    "lua",
+    "markdown",
+    "python",
+    "typescript",
+    "vue",
+    "svelte",
+    "bash",
+  }
+
+  local config = require("nvim-treesitter.config")
+
+  local already_installed = config.get_installed()
+  local parsers_to_install = {}
+
+  for _, parser in ipairs(ensure_installed) do
+    if not vim.tbl_contains(already_installed, parser) then
+      table.insert(parsers_to_install, parser)
+    end
+  end
+
+  if #parsers_to_install > 0 then
+    treesitter.install(parsers_to_install)
+  end
+
+  local group = vim.api.nvim_create_augroup("TreesitterConfig", { clear = true })
+  vim.api.nvim_create_autocmd("FileType", {
+    group = group,
+    callback = function(args)
+      if vim.list_contains(config.get_installed(), vim.treesitter.language.get_lang(args.match)) then
+        vim.treesitter.start(args.buf)
+      end
     end,
   })
+end
+
+setup_treesitter()
